21425aa72a9857a5c8f3a639deeeb9828b83d302
ishaqkhan2003 <ishaqkhan.rahim@gmail.com>
2024-11-30 14:14:40 +0530

    List to String Array

--- Task/src/com/Utility/UtilityClass.java
+++ Task/src/com/Utility/UtilityClass.java
@@ -1,30 +1,28 @@
 package com.utility ;
 import com.exceptionhandling.DataValidationException;
-public  class UtilityClass 
-{
-	public static void checkNull(Object obj) throws DataValidationException
-	{
+public  class UtilityClass {
+	public static void checkNull(Object obj) throws DataValidationException{
 		if ( obj == null )
 		{
 			throw new DataValidationException("The input string is null.");
 		}
 	}
-	public static void checkBoundary(int length,int index) throws DataValidationException
-	{
+	public static void checkBoundary(int length,int index) throws DataValidationException{
 		if ( length < index || index < 0)
 		{
 			throw new DataValidationException("Out of Index ");
 		}
 	}
-	public static int getLength(String string) throws DataValidationException
-	{
+	public static int getLength(String string) throws DataValidationException{
 		checkNull(string);
 		return string.length();
 	}
-	public static int getLength(StringBuilder string) throws DataValidationException
-	{
+	public static int getLength(StringBuilder string) throws DataValidationException{
 		checkNull(string);
 		return string.length();
+	}
+	public static int getLength(String[] strArray){
+		return strArray.length;
 	}
 
 }
--- Task/src/com/string/stringtask/StringTask.java
+++ Task/src/com/string/stringtask/StringTask.java
@@ -2,21 +2,17 @@ package com.string.stringtask;
 import java.util.*;
 import com.exceptionhandling.DataValidationException;
 import com.utility.UtilityClass;
-public class StringTask 
-{ 
-	public int getLength(String string) throws DataValidationException
-	{
+public class StringTask { 
+	public int getLength(String string) throws DataValidationException{
 		UtilityClass.checkNull(string);
 		return UtilityClass.getLength(string);	
 	}
 
-	public char[] toCharacterArray(String string) throws DataValidationException
-	{
+	public char[] toCharacterArray(String string) throws DataValidationException{
 		UtilityClass.checkNull(string);
 		return string.toCharArray();	
 	}
-	public char getCharFromLast(String string,int positionOfChar) throws DataValidationException
-	{  	
+	public char getCharFromLast(String string,int positionOfChar) throws DataValidationException{  	
 		int stringLength = UtilityClass.getLength(string);
 		if ( stringLength == 0)
 		{
@@ -26,8 +22,7 @@ public class StringTask
 		return string.charAt(stringLength - positionOfChar);
 		
 	}
-	public int numberOfOccurance(String string , char character) throws DataValidationException
-	{
+	public int numberOfOccurance(String string , char character) throws DataValidationException{
 		int stringLength = UtilityClass.getLength(string);
 		if ( stringLength == 0)
 		{
@@ -45,70 +40,57 @@ public class StringTask
 		 
 	}
 
-	public int getGreatestPosition(String string,char character) throws DataValidationException
-	{
+	public int getGreatestPosition(String string,char character) throws DataValidationException{
 		UtilityClass.checkNull(string);
 		return string.lastIndexOf(character);
 	}
-	public String getLastCharacters(String string,int noOfChar) throws DataValidationException
-	{
+	public String getLastCharacters(String string,int noOfChar) throws DataValidationException{
 		int stringLength = UtilityClass.getLength(string);
 		UtilityClass.checkBoundary(stringLength,noOfChar);
 		return string.substring((stringLength - noOfChar));
 	}
-	public String getFirstCharacters(String string,int noOfChar) throws DataValidationException
-	{
+	public String getFirstCharacters(String string,int noOfChar) throws DataValidationException{
 		int stringLength = UtilityClass.getLength(string);
 		UtilityClass.checkBoundary(stringLength,noOfChar);
 		return string.substring(0,noOfChar);
 	}
-	public String replaceFromPrefix(String string,String replacementString,int noOfChar) throws DataValidationException
-	{
+	public String replaceFromPrefix(String string,String replacementString,int noOfChar) throws DataValidationException{
 		int stringLength = UtilityClass.getLength(string);
 		UtilityClass.checkBoundary(stringLength,noOfChar);
 		UtilityClass.checkNull(replacementString);
 		return string.replaceFirst(getFirstCharacters(string,noOfChar),replacementString);
 	}
-	public String replaceFromSuffix(String string,String replacementString,int noOfChar) throws DataValidationException
-	{
+	public String replaceFromSuffix(String string,String replacementString,int noOfChar) throws DataValidationException{
 		int stringLength = UtilityClass.getLength(string);
 		UtilityClass.checkBoundary(stringLength,noOfChar);
 		UtilityClass.checkNull(replacementString);
 		return getFirstCharacters(string , getLength(string)-noOfChar)+replacementString;
 	}
-	public String replaceFromAnyIndex(String string,String replacementString,int noOfChar,int index) throws DataValidationException
-	{
+	public String replaceFromAnyIndex(String string,String replacementString,int noOfChar,int index) throws DataValidationException{
 		int stringLength = UtilityClass.getLength(string);
 		UtilityClass.checkBoundary(stringLength,noOfChar);
 		UtilityClass.checkNull(replacementString);
 		return string.replace(string.substring(index,noOfChar+index),replacementString);
-	}
-		
-		
-	public boolean checkStartsWith(String string,String startingWith) throws DataValidationException
-	{
+	}		
+	public boolean checkStartsWith(String string,String startingWith) throws DataValidationException{
 		UtilityClass.checkNull(string);
 		UtilityClass.checkNull(startingWith);
 		return string.startsWith(startingWith);	
 	}
-	public boolean checkEndsWith(String string,String endingString) throws DataValidationException 
-	{
+	public boolean checkEndsWith(String string,String endingString) throws DataValidationException {
 		UtilityClass.checkNull(string);
 		UtilityClass.checkNull(endingString);
 		return string.endsWith(endingString);
 	}
-	public String toLowerCasedString(String string) throws DataValidationException 
-	{
+	public String toLowerCasedString(String string) throws DataValidationException {
 		UtilityClass.checkNull(string);
 		return string.toLowerCase();
 	}
-	public String toUpperCasedString(String string) throws DataValidationException 
-	{
+	public String toUpperCasedString(String string) throws DataValidationException {
 		UtilityClass.checkNull(string);
 		return string.toUpperCase();
 	}
-	public String toReverseString(String string) throws DataValidationException 
-	{
+	public String toReverseString(String string) throws DataValidationException {
 		int inputstringLength = UtilityClass.getLength(string);
 		char[] inputCharArray = toCharacterArray(string);
 		char[] reverseCharArray = new char[inputstringLength];
@@ -120,19 +102,16 @@ public class StringTask
 		}
 		return new String(reverseCharArray);		
 	}
-	public String getMultipleString(String string ) throws DataValidationException
-	{
+	public String getMultipleString(String string ) throws DataValidationException{
 		UtilityClass.checkNull(string);
 		return string;
 	}
 
-	public String[] toConvertStringArray(String string,String delimittingCharacter) throws DataValidationException
-	{
+	public String[] toConvertStringArray(String string,String delimittingCharacter) throws DataValidationException{
 		UtilityClass.checkNull(string);
 		return string.split(delimittingCharacter);
 	}
-	public String concatStrings( List<String> stringArray, String sepCharacter) throws DataValidationException
-	{
+	public String concatStrings( List<String> stringArray, String sepCharacter) throws DataValidationException{
 		int sizeOfArray = stringArray.size();
 		if ( sizeOfArray == 0 )
 		{
@@ -141,25 +120,21 @@ public class StringTask
 		return String.join(sepCharacter,stringArray);
 
 	}
-	public int compareCaseSensitiveStrings(String  strOne, String strTwo) throws DataValidationException
-	{
+	public int compareCaseSensitiveStrings(String  strOne, String strTwo) throws DataValidationException{
 		UtilityClass.checkNull(strOne);
 		UtilityClass.checkNull(strTwo);
 		return strOne.compareTo(strTwo);
 	}
-	public String changeDelimitter(String string,String oldDelimittingChar,String newDelimittingChar) throws DataValidationException
-	{
+	public String changeDelimitter(String string,String oldDelimittingChar,String newDelimittingChar) throws DataValidationException{
 		UtilityClass.checkNull(string);
 		return string.replaceAll(oldDelimittingChar,newDelimittingChar);
 	}
-	public int compareCaseInSensitiveStrings(String  strOne, String strTwo) throws DataValidationException
-	{
+	public int compareCaseInSensitiveStrings(String  strOne, String strTwo) throws DataValidationException{
 		UtilityClass.checkNull(strOne);
 		UtilityClass.checkNull(strTwo);
 		return strOne.compareToIgnoreCase(strTwo);
 	}
-	public String removeWhiteSpace(String string) throws DataValidationException
-	{
+	public String removeWhiteSpace(String string) throws DataValidationException{
 		UtilityClass.checkNull(string);
 		return string.trim();
 	}
